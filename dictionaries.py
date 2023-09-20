b15b68a89c215df2eda9a9c4a344238132b9afb2
allan <allanmathenge2@gmail.com>
2023-09-21 02:41:26 +0300

    first commit

--- dictionaries.py
+++ dictionaries.py
@@ -119,10 +119,64 @@ print(len(nums))
 print(nums)
 
 # True is a dupe of 1, 0 dupe of False
-nums = {1, True, 2, False, 3, 4, 0}
+# Value that comes first gets printed 1/True, 0/False
+nums = {True, 2, 1, False, 0, 3, 4}
 print(nums)
 
 # Check if a value is in a set
 print(2 in nums)
 
-# but you cannot refer to an element in a set with an index or a key
+# You cannot refer to an element in a set with an index or a key
+
+# Add new element to a set
+nums.add(0)
+print(nums)
+
+# Add elements from one set to another
+morenums = {5, 6, 7}
+nums.update(morenums)
+print(nums)
+
+# You can update sets with lists, tuples and dictionaries
+
+# set1 = set((True, 2, 3, 4, False))
+# set2 = {0, False, "m"}
+# set1.update(set2)
+# list1 = list((5, 6, 7, 8))
+# set1.update(list1)
+# print(set1)
+# tuple1 = tuple((9, 20, 30, 40))
+# print(type(tuple1) == tuple)
+# set1.update(tuple1)
+# dict1 = {"guitar": "chords"}
+# set1.update(dict1)
+# print("guitar" in dict1)
+# print(set1)
+
+one = {1, 2, 3}
+two = {4, 5, 6}
+mynewset = one.union(two)
+print(mynewset)
+
+# Keep only the duplicates
+one = {3, 4, 5}
+two = {41, 52, 6}
+one.intersection_update(two)
+print(one)
+
+# Keep everything except the duplicates
+one = {1, 2, 3}
+two = {4, 5, 6}
+one.symmetric_difference_update(two)
+print(one)
+
+two = {7, 8, 9, 0}
+three = {4, 5, 6, 7, 8}
+# two.update(three)
+# print(two)
+# two.union(three)
+# print(two)
+# two.intersection_update(three)
+# print(two)
+two.symmetric_difference_update(three)
+print(two)
