1faf12020e1c4b661676c789aaa35beb1472ffc4
allan <allanmathenge2@gmail.com>
2023-09-27 02:47:46 +0300

    first commit

---
+++ modules.py
@@ -0,0 +1,19 @@
+from math import pi as π
+import sys
+import random as rdm
+from enum import Enum
+import kansas
+from rps7 import rock_paper_scissors
+
+print(π)
+rdm.choice("123")
+
+# for item in dir(rdm):
+#     print(item)
+
+print(kansas.capital)
+kansas.randomfacts()
+
+print(__name__)
+print(kansas.__name__)
+rock_paper_scissors()
